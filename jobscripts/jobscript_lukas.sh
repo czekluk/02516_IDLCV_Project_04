39ccd2b054420d4cd05d9151a544e3e0b50484bc
lczekal <lukas_czekalla@gmx.de>
2024-11-29 23:46:11 +0100

    two-stream networks

--- jobscripts/jobscript_lukas.sh
+++ jobscripts/jobscript_lukas.sh
@@ -18,10 +18,10 @@
 ### ------------- specify wall-clock time (max allowed is 12:00)---------------- 
 #BSUB -W 12:00
 
-#BSUB -o /zhome/c4/e/203768/02516_IDLCV_Project_03/hpc_logs/OUTPUT_FILE%J.out
-#BSUB -e /zhome/c4/e/203768/02516_IDLCV_Project_03/hpc_logs/OUTPUT_FILE%J.err
+#BSUB -o /zhome/c4/e/203768/02516_IDLCV_Project_04/hpc_logs/OUTPUT_FILE%J.out
+#BSUB -e /zhome/c4/e/203768/02516_IDLCV_Project_04/hpc_logs/OUTPUT_FILE%J.err
 
 module load python3/3.11.9
 
 source /zhome/c4/e/203768/IDLCV/bin/activate
-python /zhome/c4/e/203768/02516_IDLCV_Project_03/src/main.py
+python /zhome/c4/e/203768/02516_IDLCV_Project_04/src/main.py
